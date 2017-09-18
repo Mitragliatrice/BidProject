5e1ba6b98e7fdb6f93ae9f9100c4900d2a427702
Colton Mathews <mathews.colton@gmail.com>
2017-09-18 14:51:53 -0600

    Devise installed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,4 +10,7 @@ class StaticPagesController < ApplicationController
 
   def about_us
   end
+
+  def contact_us
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,27 @@
+# == Route Map
+#
+#       Prefix Verb URI Pattern             Controller#Action
+#     director GET  /director(.:format)     static_pages#landing
+#         home GET  /home(.:format)         static_pages#home
+# how_it_works GET  /how_it_works(.:format) static_pages#how_it_works
+#     about_us GET  /about_us(.:format)     static_pages#about_us
+#   contact_us GET  /contact_us(.:format)   static_pages#contact_us
+#         root GET  /                       static_pages#landing
+# 
+
 Rails.application.routes.draw do
-  get 'static_pages/landing'
+  devise_for :users
+  get 'director', to: 'static_pages#landing'
 
-  get 'static_pages/home'
+  get 'home', to: 'static_pages#home'
 
-  get 'static_pages/how_it_works'
+  get 'how_it_works', to: 'static_pages#how_it_works'
 
-  get 'static_pages/about_us'
+  get 'about_us', to: 'static_pages#about_us'
+
+  get 'contact_us', to: 'static_pages#contact_us'
+
+  root to: 'static_pages#landing'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
